817b5836b2d8e4ba54c6539856198db46d822a45
juniomelos <you@example.com>
2020-05-07 10:31:53 -0300

    exercise 2.4

--- javaScript-ES6/day-8.4/part-2-4.js
+++ javaScript-ES6/day-8.4/part-2-4.js
@@ -1,14 +1,22 @@
+const assert = require('assert');
+
 function secondThirdSmallest(array) {
   let results = []
   array.sort(function (x, y) {
-      return x + y;
+    console.log(x+y)
+
+      return x - y;
   });
   results = [array[1], array[2]];
+  console.log(results)
   return results;
 };
 
 const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
 const result = [5, 6];
+
+assert.strictEqual(typeof secondThirdSmallest, 'function');
+assert.deepStrictEqual(secondThirdSmallest(parameter), result);
 /*
 Use a variável parameter como parâmetro da função acima, escreva testes
 para verificar se a mesma está retornando a como se vê na variável result
