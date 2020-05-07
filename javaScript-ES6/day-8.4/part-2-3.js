8d743147c4958c539f6a034227711cd2fedc47ee
juniomelos <you@example.com>
2020-05-07 10:09:02 -0300

    exercise 2.3

--- javaScript-ES6/day-8.4/part-2-3.js
+++ javaScript-ES6/day-8.4/part-2-3.js
@@ -1,15 +1,21 @@
+const assert = require('assert');
+
 const greaterThanTen = (array) => {
-  let results = 0;
+  let results = [];
   for (let i = 0; i < array.length; i += 1) {
     if (array[i] > 10) {
-      results += array[i];
+      results.push (array[i]);
     }
   }
+  console.log(results);
   return results;
 };
 
 const parameter = [4, 10, 32, 9, 21];
 const result = [32, 21];
+
+assert.strictEqual(typeof greaterThanTen, 'function');
+assert.deepStrictEqual(greaterThanTen(parameter), result);
 /*
   Use a variável parameter como parâmetro da função acima, escreva testes
   para verificar se a mesma está retornando a como se vê na variável result
