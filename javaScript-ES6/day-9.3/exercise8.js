00785e21e856c7cbd159f2785b0ad90338604247
juniomelos <you@example.com>
2020-05-12 21:02:11 -0300

    Exercises 8

--- javaScript-ES6/day-9.3/exercise8.js
+++ javaScript-ES6/day-9.3/exercise8.js
@@ -1,6 +1,7 @@
 const assert = require('assert')
 
 // escreva greet abaixo
+const greet = (name, mensagem = 'Hi') => `${mensagem} ${name}`
 
 assert.equal(greet("John"), "Hi John")
 assert.equal(greet("John", "Good morning"), "Good morning John")
