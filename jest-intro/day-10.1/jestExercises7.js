a054745e163eca8956fdb53602c23f86f2ae1e7c
juniomelos <you@example.com>
2020-05-19 10:08:28 -0300

    Done exercises: thereIs...

--- jest-intro/day-10.1/jestExercises7.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert');
-
-const thereIs = '';
-
-// implemente seus testes aqui
-
-assert.notEqual(thereIs, undefined, 'variable thereIs must not be undefined');
---
+++ jest-intro/day-10.1/thereIs.test.js
@@ -0,0 +1,8 @@
+const thereIs = require('./thereIs');
+
+describe('Teste thereIs', () => {
+
+  test('variable thereIs must not be undefined', () => {
+    expect(typeof(thereIs)).not.toBe('undefined');
+  })
+});
