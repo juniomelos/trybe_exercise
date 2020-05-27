4161892156e4d343c99690bbf4600e0ac94c5f96
juniomelos <you@example.com>
2020-05-26 21:19:50 -0300

    Exercise 2

--- jest-intro/day-11.2/exercises.test.js
+++ jest-intro/day-11.2/exercises.test.js
@@ -1,10 +1,20 @@
 const exercises = require('./exercises');
 
-// Exercise 1
+/* // Exercise 1
 test('Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.', () => {
   exercises.randomNumber = jest.fn().mockReturnValue(10);
 
   expect(exercises.randomNumber()).toBe(10);
+  expect(exercises.randomNumber).toHaveBeenCalled();
+  expect(exercises.randomNumber).toHaveBeenCalledTimes(1);
+});
+ */
+
+// Exercise 2
+test('Deve receber dois parâmetros e retornar divisão do primeiro pelo segundo. Ocorrer uma única vez.', () => {
+  exercises.randomNumber = jest.fn().mockImplementation((a, b) => a / b);
+
+  expect(exercises.randomNumber(10, 5)).toBe(2);
   expect(exercises.randomNumber).toHaveBeenCalled();
   expect(exercises.randomNumber).toHaveBeenCalledTimes(1);
 });
