1939f19710daaa0cf126b70b1280cce815de193e
juniomelos <you@example.com>
2020-05-26 21:16:39 -0300

    Exercise 1

--- jest-intro/day-11.2/exercises.test.js
+++ jest-intro/day-11.2/exercises.test.js
@@ -1,1 +1,10 @@
-const service = require('./exercises');
+const exercises = require('./exercises');
+
+// Exercise 1
+test('Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.', () => {
+  exercises.randomNumber = jest.fn().mockReturnValue(10);
+
+  expect(exercises.randomNumber()).toBe(10);
+  expect(exercises.randomNumber).toHaveBeenCalled();
+  expect(exercises.randomNumber).toHaveBeenCalledTimes(1);
+});
