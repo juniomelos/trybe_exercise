307e2c6303e6aec822d7b9cb65c1a6ee858d2abf
juniomelos <you@example.com>
2020-05-28 19:12:33 -0300

    exercises 1 and 2

--- react/day-12.1/src/App.js
+++ react/day-12.1/src/App.js
@@ -2,22 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const task = value => {
+  return <li>{value}</li>;
+};
+
+const toDoList = ['study', 'sleep', 'study', 'anime', 'study', 'drink', 'eat'];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {toDoList.map(element => task(element))}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
