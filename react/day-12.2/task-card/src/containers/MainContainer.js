aad158700d87b0109c2a3a8ee2c04037a1c49c3e
juniomelos <you@example.com>
2020-05-30 10:59:10 -0300

    updating~

---
+++ react/day-12.2/task-card/src/containers/MainContainer.js
@@ -0,0 +1,6 @@
+/* import React from 'react';
+import ToDoCart from '../components/ToDoCard';
+import CreateCard from '../components/CreateCard';
+import ToDoCardContainer from './ToDoCardContainer';
+
+ */
---
+++ react/day-12.2/task-card/src/containers/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <div className='nav'>
+      <img className='logo' src='../lista-de-controle.svg' alt='logo' />
+    </div>
+  );
+}
+
+export default Nav;
