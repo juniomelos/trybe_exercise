c2a171aa6ac461ae756469a461bc91633a4d62a2
juniomelos <you@example.com>
2020-05-30 11:16:07 -0300

    redoing project task card

--- react/day-12.2/task-card/src/containers/MainContainer.js
+++
@@ -1,6 +0,0 @@
-/* import React from 'react';
-import ToDoCart from '../components/ToDoCard';
-import CreateCard from '../components/CreateCard';
-import ToDoCardContainer from './ToDoCardContainer';
-
- */
