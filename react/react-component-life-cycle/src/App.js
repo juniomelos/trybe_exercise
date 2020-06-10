249a41265cbe81d544a1a64ed0c00544742fd807
juniomelos <juniomelos@gmail.com>
2020-06-10 10:59:39 -0300

    Exercises 1, 2 and 3

--- react/react-component-life-cycle/src/App.js
+++ react/react-component-life-cycle/src/App.js
@@ -6,27 +6,39 @@ class App extends React.Component {
     this.state = {
       data: "",
     };
-    this.fetchDog = this.fetchDog.bind(this);
   }
 
   componentDidMount() {
     this.fetchDog();
   }
 
-  fetchDog() {
-    fetch("https://dog.ceo/api/breeds/image/random")
-      .then((res) => res.json())
-      .then((result) => this.setState({ data: result }));
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.data.message.includes("terrier")) {
+      return false;
+    }
+    return true;
   }
+
+  componentDidUpdate() {
+    localStorage.setItem("dogURL", this.state.data.message);
+    const dogBreed = this.state.data.message.split("/")[4];
+    alert(`Dog Breed: ${dogBreed}`);
+  }
+
+  fetchDog = async () => {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const result = await response.json();
+    this.setState({ data: result });
+  };
 
   render() {
     if (this.state.data === "") return "loading...";
     return (
       <div>
-        <p>Doguinhos</p>
-        <button onClick={this.fetchDog}>Novo doguinho!</button>
+        <p>Puppy Image Gallery</p>
+        <button onClick={this.fetchDog}>New dog</button>
         <div>
-          <img alt='dog' src={this.state.data.message} />
+          <img alt="dog" src={this.state.data.message} />
         </div>
       </div>
     );
