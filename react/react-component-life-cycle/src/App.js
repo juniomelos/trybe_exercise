a865e2b92a13f7280f81546276367fe847d7c6d0
juniomelos <juniomelos@gmail.com>
2020-06-10 23:17:24 -0300

    Bonus totally driven by resolution

--- react/react-component-life-cycle/src/App.js
+++ react/react-component-life-cycle/src/App.js
@@ -5,10 +5,18 @@ class App extends React.Component {
     super(props);
     this.state = {
       data: "",
+      name: "",
+      array: [],
     };
   }
 
   componentDidMount() {
+    if (localStorage.dogURL) {
+      const parseStorage = JSON.parse(localStorage.dogURL);
+      console.log(parseStorage);
+      const lastDog = parseStorage[parseStorage.length - 1].message;
+      return this.setState({ data: { message: lastDog } });
+    }
     this.fetchDog();
   }
 
@@ -19,16 +27,30 @@ class App extends React.Component {
     return true;
   }
 
-  componentDidUpdate() {
-    localStorage.setItem("dogURL", this.state.data.message);
-    const dogBreed = this.state.data.message.split("/")[4];
-    alert(`Dog Breed: ${dogBreed}`);
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.data !== this.state.data) {
+      const dogBreed = this.state.data.message.split("/")[4];
+      return alert(`Dog Breed: ${dogBreed}`);
+    }
+    localStorage.setItem("dogURL", JSON.stringify(this.state.array));
   }
 
   fetchDog = async () => {
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
     const result = await response.json();
     this.setState({ data: result });
+  };
+
+  saveDog = () => {
+    const {
+      data: { message },
+      name,
+      array,
+    } = this.state;
+    const dogData = { message, name };
+    const newArray = [...array, dogData];
+    this.setState({ array: newArray });
+    this.setState({ name: "" });
   };
 
   render() {
@@ -38,7 +60,16 @@ class App extends React.Component {
         <p>Puppy Image Gallery</p>
         <button onClick={this.fetchDog}>New dog</button>
         <div>
-          <img alt="dog" src={this.state.data.message} />
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={(e) => this.setState({ name: e.target.value })}
+            placeholder="dog name"
+          />
+          <button onClick={this.saveDog}>Save Dog</button>
+        </div>
+        <div>
+          <img width="30%" alt="dog" src={this.state.data.message} />
         </div>
       </div>
     );
